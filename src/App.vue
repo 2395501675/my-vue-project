df6ff68f378662dae3ebf575a5d9d7da9f12f1c6
lnn <liningning@adpanshi.com>
2020-03-23 12:51:19 +0800

    tij

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <hello-world v-model="msg" @change-msg="addMsg"></hello-world>
+    <message :name.sync="msg">
+      <template v-slot:title>警告框{{msg}}</template>
+    </message>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from '@/components/HelloWorld.vue'
+import message from '@/components/message.vue'
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    message
+  },
+  data() {
+    return {
+      msg: 'hi lnn'
+    }
+  },
+  methods: {
+    addMsg(value) {
+      console.log(value)
+    }
+  },
 }
 </script>
-
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -23,6 +39,20 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+input{
+ color:red;
+}
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/components/message.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div>欢迎：{{name}}</div>
+    <slot name="title"></slot>
+    <button @click="$emit('update:name', 'lnn')">提交</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
